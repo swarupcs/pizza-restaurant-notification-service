4020c370a139160e5232dde5339021db93746fbe
Swarup Das <swarupcs@gmail.com>
2026-04-07 02:10:53 +0000

    make secure true for port 465

--- src/mail.ts
+++ src/mail.ts
@@ -9,7 +9,7 @@ export class MailTransport implements NotificationTransport {
     this.transporter = nodemailer.createTransport({
       host: config.get("mail.host"),
       port: config.get("mail.port"),
-      secure: false, // Use `true` for port 465, `false` for all other ports
+      secure: true, // Use `true` for port 465, `false` for all other ports
       auth: {
         user: config.get("mail.auth.user"),
         pass: config.get("mail.auth.pass"),
